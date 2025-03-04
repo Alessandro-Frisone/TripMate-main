07fe5ff444f03f4b133791713353ae0453fa59bb
Alessandro Frisone <alefri2004@gmail.com>
2025-03-04 20:50:32 +0100

    feat: add trip data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 
 export default function Header() {
   return (
-    <header className="fixed left-0 top-0 h-screen w-60 bg-gray-900 text-white p-6 shadow-2xl border-r border-gray-700 flex flex-col justify-start">
+    <header className="col-span-3 bg-gray-800 text-white p-4">
       <NavBar />
     </header>
   );
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 
-export default function DefaultLayout(){
-    return (
-        <>
-          <Header/>
-          <main className="ml-60 p-6 flex-1">
-            <Outlet />
-          </main>
-        </>
-      )
+export default function DefaultLayout() {
+  return (
+    <div className="grid grid-cols-12 h-screen">
+      <Header />
+      <main className="col-span-9 p-6 bg-white">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
